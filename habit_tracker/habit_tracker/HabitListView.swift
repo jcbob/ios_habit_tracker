c7042071d9534fae56d3f9a9c27d7b8859b0735e
Wojciech Wolski <thorton@MacBook-Pro-Wojciech.local>
2023-09-11 14:43:11 +0200

    created the project and added HabitListView SwiftUI file

---
+++ habit_tracker/habit_tracker/HabitListView.swift
@@ -0,0 +1,20 @@
+//
+//  HabitListView.swift
+//  habit_tracker
+//
+//  Created by Wojciech Wolski on 11/09/2023.
+//
+
+import SwiftUI
+
+struct HabitListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HabitListView_Previews: PreviewProvider {
+    static var previews: some View {
+        HabitListView()
+    }
+}
---
+++ habit_tracker/habit_tracker/habit_trackerApp.swift
@@ -0,0 +1,20 @@
+//
+//  habit_trackerApp.swift
+//  habit_tracker
+//
+//  Created by Wojciech Wolski on 11/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct habit_trackerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
